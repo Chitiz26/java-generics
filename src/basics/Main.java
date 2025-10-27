23b09ab1063f4ab11475b5d5fd49418be5d7402f
chitizawasthi <chitizawasthi@gmail.com>
2025-10-27 20:04:07 +0530

    add basics of java generics

---
+++ src/basics/Main.java
@@ -0,0 +1,13 @@
+package basics;
+
+import java.util.ArrayList;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> list=new ArrayList<>();
+        list.add("abcd");
+        System.out.println(list.get(0));
+    }
+}
---
+++ src/basics/Main2.java
@@ -0,0 +1,9 @@
+package basics;
+public class Main2
+{
+    public static void main(String[] args)
+    {
+        Item<Integer> obj=new Item();
+        obj.setValue(1);
+    }
+}
